bb1604d281f2a21fff636e4cc85286aa36a34db5
Rvitnwer <rvitnwer@gmail.com>
2024-07-22 18:31:14 +0530

    changes

--- src/screens/home-screen.js
+++ src/screens/home-screen.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import Header from '~/components/header/header';
+import AboutUs from '~/components/home/about-us';
+import Banner from '~/components/home/banner';
+import CopperHome from '~/components/home/copper-home';
 
 const HomeScreen = () => {
     return (
         <div>
             <Header />
-            HomeScreen
-            <div className="Heading">Heading</div>
+            <Banner />
+            <AboutUs />
+            <CopperHome />
         </div>
     );
 };
